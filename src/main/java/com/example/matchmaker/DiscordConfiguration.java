b1b8b120f4c4a4dcde33a0aefeb883cb83c62982
rhtld123 <rhtld123@gmail.com>
2024-08-31 20:37:11 +0900

    feat: 팀 나누는 기능 추가

--- src/main/java/com/example/matchmaker/DiscordConfiguration.java
+++ src/main/java/com/example/matchmaker/DiscordConfiguration.java
@@ -2,6 +2,7 @@ package com.example.matchmaker;
 
 import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
 import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_VOICE_STATES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.MESSAGE_CONTENT;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
@@ -22,9 +24,15 @@ public class DiscordConfiguration {
   private void init() {
     log.info("initialize JDA...");
     JDA jda = JDABuilder.createDefault(discordProperties.getToken())
-        .setActivity(Activity.playing("팀 짜려고 기다리는 중.."))
-        .enableIntents(GUILD_MESSAGES, GUILD_VOICE_STATES)
+        .setActivity(Activity.playing("내전 대기"))
+        .enableIntents(GUILD_MESSAGES, GUILD_VOICE_STATES, MESSAGE_CONTENT)
         .addEventListeners(new DiscordEventListener())
         .build();
+
+    jda.updateCommands().addCommands(Commands.slash(Constants.COMMAND, "팀을 나눕니다.")).complete();
+  }
+
+  public static class Constants {
+    public static final String COMMAND = "팀나누기";
   }
 }
--- src/main/java/com/example/matchmaker/DiscordEventListener.java
+++ src/main/java/com/example/matchmaker/DiscordEventListener.java
@@ -1,17 +1,61 @@
 package com.example.matchmaker;
 
+import com.example.matchmaker.DiscordConfiguration.Constants;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 @Slf4j
 public class DiscordEventListener extends ListenerAdapter {
 
+  public static final int SHUFFLE_COUNT = 3;
+
   @Override
-  public void onMessageReceived(MessageReceivedEvent event) {
+  public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
     if (!event.isFromGuild()) {
-      log.error("채널에서 온 메시지가 아님");
       return;
     }
+    if (!Constants.COMMAND.equals(event.getName())) {
+      event.reply("아직 지원하지 않는 명령어 입니다.").queue();
+    }
+
+    Member chatMember = event.getMember();
+    Guild guild = event.getGuild();
+    List<VoiceChannel> voiceChannels = guild.getVoiceChannels();
+
+    if (voiceChannels.size() < 2) {
+      event.reply("음성 채널을 최소 2개 이상 생성해 주세요.").queue();
+      return;
+    }
+    GuildVoiceState voiceState = chatMember.getVoiceState();
+
+    if (voiceState == null || voiceState.getChannel() == null) {
+      event.reply("음성 채널에 접속해 주세요.").queue();
+      return;
+    }
+    VoiceChannel authorVoiceChannel = guild.getVoiceChannelById(voiceState.getChannel().getId());
+    List<Member> members = new ArrayList<>(authorVoiceChannel.getMembers());
+
+    for (int i = 0; i < SHUFFLE_COUNT; i++) {
+      Collections.shuffle(members);
+    }
+
+    for (int i = 0; i < members.size(); i++) {
+      Member member = members.get(i);
+      if (i % 2 == 0) {
+        guild.moveVoiceMember(member, voiceChannels.get(0)).queue();
+        continue;
+      }
+      guild.moveVoiceMember(member, voiceChannels.get(1)).queue();
+    }
+
+    event.reply("팀 나누기가 완료되었습니다.").queue();
   }
 }
