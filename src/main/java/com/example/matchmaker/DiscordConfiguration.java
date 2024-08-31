746d99fa2a27fe0b579f5f56cba37db7fca17e36
rhtld123 <rhtld123@gmail.com>
2024-08-31 12:02:02 +0900

    chore: logging 추가

--- src/main/java/com/example/matchmaker/DiscordConfiguration.java
+++ src/main/java/com/example/matchmaker/DiscordConfiguration.java
@@ -5,11 +5,13 @@ import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_VOICE_STATES;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class DiscordConfiguration {
@@ -17,7 +19,8 @@ public class DiscordConfiguration {
   private final DiscordProperties discordProperties;
 
   @PostConstruct
-  public void init() {
+  private void init() {
+    log.info("initialize JDA...");
     JDA jda = JDABuilder.createDefault(discordProperties.getToken())
         .setActivity(Activity.playing("팀 짜려고 기다리는 중.."))
         .enableIntents(GUILD_MESSAGES, GUILD_VOICE_STATES)
